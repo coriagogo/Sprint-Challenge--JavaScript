3dd6c4dadbb42521a479346388ed446b84432fe4
coriagogo <pkcparis@gmail.com>
2019-04-05 12:12:38 -0400

    objects and functions added except problem 3 on arrays

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,26 +6,54 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function firstItem(param1, param2, cb) {
+  return cb(param1, param2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+//Sum of two numbers
+function add (x, y, cb) {
+  return cb(x + y);
+}
 
+add(2, 2, function (addAnswer) {
+  console.log(addAnswer)
+});
+
+
+//Product of two numbers
+function multiply(x, y, cb) {
+  return cb(x * y);
+}
+
+multiply(10, 16, function(multiplyAnswer) {
+  console.log(multiplyAnswer)
+});
+
+
+//Greeting
+function greeting(firstName, lastName, cb) {
+  return cb(`Hello ${firstName} ${lastName}, nice to meet you!`);
+}
+greeting("Mary", "Poppins", function(greetingString) {
+  console.log(greetingString)
+});
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // consume(2,2,add); // 4
 // consume(10,16,multiply); // 160
 // consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
-
+/* Tested with functions */
 
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: `nestedfunction()` can access the variable `internal` because it's being invoked inside the nesting.
 
 
 const external = "I'm outside the function";
